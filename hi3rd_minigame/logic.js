b7e62a9dead959cbe8a50b863b2ff092f4f40fa5
SpudWumper <rebornyuujinchou@gmail.com>
2024-11-09 00:33:20 -0600

    setting up some more files

---
+++ hi3rd_minigame/logic.js
@@ -0,0 +1,6 @@
+//handle all the game logic with javascript
+//and use DOM factory methods, etc for basic menu structure and stuff like that
+
+document.addEventListener("DOMContentLoaded", function() {
+
+})
